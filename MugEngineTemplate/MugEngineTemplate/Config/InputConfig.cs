d289a742cce9d51d19f1c074b3d6f08403596cd3
August Dahlkvist <AugustDahlkvist@gmail.com>
2025-07-31 19:00:19 +0200

    Remove debug code

--- MugEngineTemplate/MugEngineTemplate/Config/InputConfig.cs
+++ MugEngineTemplate/MugEngineTemplate/Config/InputConfig.cs
@@ -6,23 +6,10 @@
 		MoveRight,
 		Jump,
 		Confirm,
-
-#if DEBUG
-		DebugCameraToggle,
-		DebugCameraZoom,
-		DebugCameraUp,
-		DebugCameraDown,
-		DebugCameraLeft,
-		DebugCameraRight
-#endif // DEBUG
 	}
 
 	static class InputConfig
 	{
-#if DEBUG
-		public static readonly MDirPad<GInput> DebugCamDPad = new(GInput.DebugCameraUp, GInput.DebugCameraDown, GInput.DebugCameraLeft, GInput.DebugCameraRight);
-#endif // DEBUG
-
 		public static void SetDefaultButtons()
 		{
 			// Arrow keys or WASD
